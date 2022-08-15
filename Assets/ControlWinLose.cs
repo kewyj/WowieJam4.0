d740e68d078c6053887d3ada8402a9d90918907a
jiawendh <jiawendh@gmail.com>
2022-08-15 18:03:58 +0800

    gameover sound effect & bgm

--- Assets/ControlWinLose.cs
+++ Assets/ControlWinLose.cs
@@ -9,16 +9,21 @@ public class ControlWinLose : MonoBehaviour
     [SerializeField]
     GameObject GeneratorController;
 
+    private AudioSource Sound;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Sound = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update()
     {
         if (player.GetComponent<PlayerJump>().death)
+        {
+            Sound.Play();
             GeneratorController.GetComponent<ControlGeneration>().Restart();
+        }
     }
 }
